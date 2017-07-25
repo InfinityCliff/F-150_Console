415bfa43aca647c286a7c2f4041a71094e86b773
Kevin Williams <kevin.Williams@algo-fx.net>
2017-07-25 16:41:18 -0400

    7.25.17 4.40 pm

--- Common.py
+++ Common.py
@@ -3,38 +3,21 @@ from kivy.uix.popup import Popup
 from kivy.properties import ObjectProperty
 
 common_kv = """
-<SideMenu>:
-    #Button_Content: button_content
-    id: sidemenu
-    size_hint: None, 1
-    width: 150
-    pos_hint: {'x': 0}
-    x: 0
-    halign: 'center'
-    BoxLayout:
-        id: bl
-        BoxLayout:
-            id: button_content                         
-            pos_hint: {'center_y': 0.5}
-            size_hint_y: None
-            height: 90
-            size_hint_x: None
-            width: 100
-        Button:
-            text: '<<'
-            on_press:
-                sidemenu.dismiss() 
+
 """
 
 
 class SideMenu(Popup):
-    content_ = ObjectProperty(None)
-    Button_Content = ObjectProperty(None)
+    Button_Content = ObjectProperty()
+
+    def __init__(self, content_, **kwargs):
+        super(SideMenu, self).__init__(**kwargs)
+        self.content_ = content_
+        self.add_content_(self.content_)
 
     def add_content_(self, widget):
-        self.content_ = widget
-        self.ids.button_content.add_widget(self.content_)
-
+        self.Button_Content.clear_widgets()
+        self.Button_Content.add_widget(widget)
 
     def dismiss(self, *largs, **kwargs):
         self.ids.button_content.remove_widget(self.content_)
